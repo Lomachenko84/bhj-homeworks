2eca4eba92794dffb5ebb68acbb6ab8dfb40e9f4
Lomachenko84 <agent84@inbox.ru>
2022-08-14 07:21:27 +0300

    задание 2.2 исправлено

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,7 +3,7 @@ for (let i = 0; click.length > i; i++) {
     click[i].addEventListener('click', function (e) {
         if (click[i].nextElementSibling) {
             e.preventDefault()
-            click[i].nextElementSibling.classList.add('menu_active')
+            click[i].nextElementSibling.classList.toggle('menu_active')
         }
     })
 }
