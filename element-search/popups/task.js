719b4235eb429566e4cd64e6dd57f4f26673bb9e
Lomachenko84 <agent84@inbox.ru>
2022-08-18 22:45:00 +0300

    задание 1 изменения

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,12 +1,14 @@
 const main = document.getElementById('modal_main');
 main.classList.add('modal_active');
 let active = main.getElementsByClassName('modal__close modal__close_times');
-    active[0].onclick= function () {
-        main.classList.remove('modal_active')
-    }
+active[0].onclick = function () {
+    main.classList.remove('modal_active')
+
+}
 
 const success = document.getElementById('modal_success');
 let show = document.getElementsByClassName('show-success');
 show[0].onclick = function () {
     success.classList.add('modal_active');
+    this.closest('.modal').style.display = "none";
 }
