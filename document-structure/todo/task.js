490ea9fd2c7af41e06437ca4e1a2caef44ebb72e
Lomachenko84 <agent84@inbox.ru>
2022-09-21 23:09:09 +0300

    задание 2.6

---
+++ document-structure/todo/task.js
@@ -0,0 +1,28 @@
+const input = document.getElementById("task__input")
+const form = document.getElementById("tasks__form")
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    let text = input.value;
+    if (text) {
+        input.value = ""
+        let title = document.createElement('div')
+        title.classList.add("task__title")
+        title.textContent = text;
+        let task = document.createElement("div")
+        task.classList.add("task")
+        let remove = document.createElement("a")
+        remove.classList.add("task__remove")
+        remove.setAttribute('href', "#")
+        remove.innerHTML = '&times;'
+        task.appendChild(title)
+        task.appendChild(remove)
+        
+        let q = document.getElementById("tasks__list")
+        q.appendChild(task)
+        remove.addEventListener("click", function(y) {
+            y.preventDefault()
+            task.remove()
+        })
+    }
+})
+
